1f6716fe2d099b6b956bd9ae6f2b3c7f7c438fbe
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-03-08 11:58:25 +0200

    set up my servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,33 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Install nginx and create folders
 
-apt-get update
-apt-get install -y nginx
+#Install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+#Create necessary folders
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+#Add content to index.html
+echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://github.com/stanley-nyoni;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+#Create a symbolic link
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-service nginx restart
+#Change ownership of /data/
+sudo chown -R ubuntu:ubuntu /data/
+
+#Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-enabled/default
+
+#Restart nginx
+sudo service nginx restart
